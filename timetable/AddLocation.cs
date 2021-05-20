453d5f06036f189d5597a319b12eff0e332bc07d
unknown <isururaveen4520@gmail.com>
2021-05-20 14:08:57 +0530

    add location clear button

--- timetable/AddLocation.cs
+++ timetable/AddLocation.cs
@@ -59,6 +59,16 @@ namespace TimeTableM
             con.Close();
         }
 
+        //clear Button click
+		private void button3_Click(object sender, EventArgs e)
+        {
+            cmbbuildingName.Text = string.Empty;
+            txtroomName.Clear();
+            roomType = string.Empty;
+            
+
+        }
+
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
             roomType = "Lecture Hall";
