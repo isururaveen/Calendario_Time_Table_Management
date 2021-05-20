ba40c51f85f5e0a0f9b34f609c82d9de7ebec474
unknown <isururaveen4520@gmail.com>
2021-05-20 14:44:00 +0530

    Add LoadstatDetails function in Statistics

--- timetable/Statistic.cs
+++ timetable/Statistic.cs
@@ -14,9 +14,11 @@ namespace TimeTableM
     {
         public Statistic()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            loadStatDetails();
         }
 
+        //home Button clicked in Dashboard
         private void button1_Click(object sender, EventArgs e)
         {
             Home Home = new Home();
@@ -32,6 +34,73 @@ namespace TimeTableM
         private void label10_Click(object sender, EventArgs e)
         {
 
+        }
+
+        //Load Statistics Details - Count
+        public void loadStatDetails()
+        {
+            con.Open();
+            try
+            {
+                string qu = "SELECT COUNT(EmpID) FROM addlecture";
+                MySqlCommand cm = new MySqlCommand(qu, con);
+                Int32 rcount = Convert.ToInt32(cm.ExecuteScalar());
+                cm.Dispose();
+
+                label6Reg.Text = rcount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+
+            con.Open();
+            try
+            {
+                string qu = "SELECT COUNT(GrpNo) FROM addstudentgrp";
+                MySqlCommand cm = new MySqlCommand(qu, con);
+                Int32 rcount = Convert.ToInt32(cm.ExecuteScalar());
+                cm.Dispose();
+
+                labelStudentGroups.Text = rcount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+
+            con.Open();
+            try
+            {
+                string qu = "SELECT COUNT(SubjectCode) FROM addsubject";
+                MySqlCommand cm = new MySqlCommand(qu, con);
+                Int32 rcount = Convert.ToInt32(cm.ExecuteScalar());
+                cm.Dispose();
+
+                labelmodules.Text = rcount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            con.Close();
+
+            con.Open();
+            try
+            {
+                string qu = "SELECT COUNT(roomName) FROM addlocation";
+                MySqlCommand cm = new MySqlCommand(qu, con);
+                Int32 rcount = Convert.ToInt32(cm.ExecuteScalar());
+                cm.Dispose();
+
+                labelLocations.Text = rcount.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
